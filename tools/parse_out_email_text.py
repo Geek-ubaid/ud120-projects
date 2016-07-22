9e6f8e6dd2c6c541f610f4e1824419ea37349106
lttazz99 <lttazz99@gmail.com>
2016-07-22 07:36:04 +0530

    .

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -26,18 +26,13 @@ def parseOutText(f):
     if len(content) > 1:
         ### remove punctuation
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
-
         ### project part 2: comment out the line below
-        words = text_string
-
+        #words = text_string
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
-
-
-
-
+        stemmer = SnowballStemmer("english")                
+        words = re.sub('([\n\t\b]+|[ ]{2,})', ' ', ' '.join(map(stemmer.stem, filter(lambda x: len(x) > 0, text_string.split()))))
     return words
 
     
@@ -51,4 +46,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
