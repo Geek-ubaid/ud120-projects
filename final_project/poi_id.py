73c40e67d6f676dc55fe0ec9197a77965aa6f89f
lttazz99 <lttazz99@gmail.com>
2016-07-27 00:28:32 +0530

    added 2 custom features, grid search and feature scaling

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
 
-import sys
+import math
 import pickle
-sys.path.append("../tools/")
+import sys
+
+from sklearn import preprocessing
+from sklearn.cross_validation import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import f1_score
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data, main
+from sklearn.metrics.scorer import f1_scorer
+
+sys.path.append("../tools/")
+
 
 # ## Task 1: Select what features you'll use.
 # ## features_list is a list of strings, each of which is a feature name.
@@ -19,7 +30,11 @@ with open("final_project_dataset.pkl", "r") as data_file:
 # ## Task 2: Remove outliers
 data_dict.pop("TOTAL", 0)
 # ## Task 3: Create new feature(s)
-
+for key in data_dict.keys():
+    from_ratio = float(data_dict[key]["from_this_person_to_poi"]) / float(data_dict[key]["from_messages"])
+    to_ratio = float(data_dict[key]["from_poi_to_this_person"]) / float(data_dict[key]["to_messages"])
+    data_dict[key]["from_ratio"] = "NaN" if math.isnan(from_ratio) else from_ratio
+    data_dict[key]["to_ratio"] = "NaN" if math.isnan(to_ratio) else to_ratio
 # ## Store to my_dataset for easy export below.
 my_dataset = data_dict
 
@@ -27,9 +42,14 @@ my_dataset = data_dict
 data = featureFormat(my_dataset, features_list, sort_keys=True)
 labels, features = targetFeatureSplit(data)
 
-from sklearn.decomposition import PCA
-pca = PCA(n_components=4)
-features = pca.fit_transform(features)
+min_max_scaler = preprocessing.MinMaxScaler()
+min_max_scaler.fit(features)
+
+param_grid = {'n_components' : [1, 2, 3, 4, 5, 6]}
+
+pca = GridSearchCV(PCA(), param_grid)
+pca = pca.fit(features)
+features = pca.transform(features)
 
 # ## Task 4: Try a varity of classifiers
 # ## Please name your classifier clf for easy export below.
@@ -38,9 +58,13 @@ features = pca.fit_transform(features)
 # ## http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.tree import DecisionTreeClassifier
+# param_grid = {'criterion' : ['entropy', 'gini'], 'splitter' : ['best', 'random'], 'min_samples_split' : [2, 3, 4, 5, 6, 7, 8, 9]}
+# clf = GridSearchCV(DecisionTreeClassifier(), param_grid, scoring = f1_scorer)
 clf = DecisionTreeClassifier(criterion="gini")
-clf.fit(features, labels)
+clf = clf.fit(features, labels)
+# clf = SVC()
+# clf.fit(features, labels)
+
 
 # ## Task 5: Tune your classifier to achieve better than .3 precision and recall 
 # ## using our testing script. Check the tester.py script in the final project
@@ -50,7 +74,6 @@ clf.fit(features, labels)
 # ## http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
